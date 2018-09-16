6a877ab8eea466c1bd3472d84f939f4d639b8592
zach <zte.zachary@gmail.com>
2018-09-16 13:49:30 -0700

    Fixed errors in methods and tested delete.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,5 +1,5 @@
 var ObjectID = require('mongodb').ObjectID
-
+//
 module.exports = function(app, db) {
     app.get('/notes/:id', (req, res) => {
         const id = req.params.id;
@@ -16,8 +16,7 @@ module.exports = function(app, db) {
     app.delete('/notes/:id', (req, res) => {
         const id = req.params.id;
         const details = {'_id': new ObjectID(id)};
-        const note = { text: req.body.body, title: req.body.title };
-        db.collection('notes').update(details, note, (err, item) => {
+        db.collection('notes').remove(details, (err, item) => {
             if (err) {
                 res.send({ 'error': 'Error occured' });
             } else {
@@ -25,11 +24,12 @@ module.exports = function(app, db) {
             }
         });
     });
-
+    
     app.put('/notes/:id', (req, res) => {
         const id = req.params.id;
         const details = {'_id': new ObjectID(id)};
-        db.collection('notes').findOne(details, (err, item) => {
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').updateOne(details, note, (err, item) => {
             if (err) {
                 res.send({ 'error': 'Error occured' });
             } else {
