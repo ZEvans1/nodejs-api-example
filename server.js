35d1642a75a9ede0f9792fb800e9dfab45828f42
zach <zte.zachary@gmail.com>
2018-09-15 20:01:21 -0700

    Connect database to app.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require ('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db');
+
 
 const app = express();
 
@@ -8,7 +10,14 @@ const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-require('./app/routes')(app, {});
-app.listen(port, () => {
-    console.log("We are live on " + port);
+
+MongoClient.connect(db.url, (err, database) => {
+    if (err) {
+        return console.log(err)  
+    } else {
+        require('./app/routes')(app, database.db());
+        app.listen(port, () => {
+            console.log("We are live on " + port);
+        })
+    } 
 })
